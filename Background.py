0a71489adc2808521528f6089566fe40029fe886
Lucas Dahl <lmd742@hotmail.com>
2020-07-07 18:16:54 -0500

    Changed the use of .txt to a SQLite file
    Added the functions for a SQLite database instead of a text file for both the Background and Automated_wisher files. Allows users to insert names and dates in a database and will pull from the database the names when given the date

--- Background.py
+++ Background.py
@@ -5,13 +5,52 @@ import re
 from fbchat import Client
 from fbchat.models import *
 import datetime
-
+import sqlite3
+from sqlite3 import Error
 
 now= datetime.datetime.now()
 k=now.strftime("%d-%m")
-file = open("Database.txt", 'r')
-cont = file.read()
 
+db_file = "Database.db"
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def insert_birthday(birthday = ""):
+    
+    """ function to insert new birthdays in the database """
+    conn = create_connection(db_file)
+    with conn:
+	    day = datetime.datetime.now()
+	    day = now.strftime("%d-%m")
+	    names = "Testname"
+	    birthday = (names, day)
+	    sql = ''' INSERT INTO birthdays(name, birthday)VALUES(?,?)'''
+	    cur = conn.cursor()
+	    cur.execute(sql, birthday)
+	    return cur.lastrowid
+
+
+def select_name(date=k):
+
+    """Get name for the date given"""
+    conn = create_connection(db_file)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM birthdays WHERE birthday=?",(date,))
+        rows = cur.fetchall()
+
+    for row in rows:
+        print(row[0])
+    return rows
+
+cont = select_name(k)
 
 
 if k in cont:
